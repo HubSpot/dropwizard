8bd0e1d0bafdf0cf67fa6647ce02f254cc76f8b7
Jonathan Haber <jhaber@hubspot.com>
2018-03-23 17:06:46 -0400

    Add validation of contexts

--- dropwizard-migrations/src/main/java/io/dropwizard/migrations/DbMigrateCommand.java
+++ dropwizard-migrations/src/main/java/io/dropwizard/migrations/DbMigrateCommand.java
@@ -1,17 +1,23 @@
 package io.dropwizard.migrations;
 
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Joiner;
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Splitter;
+
 import liquibase.Liquibase;
 import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparser;
-
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 public class DbMigrateCommand extends AbstractLiquibaseCommand {
 
@@ -50,6 +56,7 @@ public class DbMigrateCommand extends AbstractLiquibaseCommand {
     @SuppressWarnings("UseOfSystemOutOrSystemErr")
     public void run(Namespace namespace, Liquibase liquibase) throws Exception {
         final String context = getContext(namespace);
+        validateContext(context, liquibase);
         final Integer count = namespace.getInt("count");
         final boolean dryRun = MoreObjects.firstNonNull(namespace.getBoolean("dry-run"), false);
         if (count != null) {
@@ -73,5 +80,37 @@ public class DbMigrateCommand extends AbstractLiquibaseCommand {
             return "";
         }
         return Joiner.on(',').join(contexts);
+    }
+
+    private static void validateContext(String context, Liquibase liquibase) {
+        Set<String> contextParts = Splitter
+            .on(',')
+            .trimResults()
+            .omitEmptyStrings()
+            .splitToList(context)
+            .stream()
+            .map(String::toLowerCase)
+            .collect(Collectors.toSet());
+
+        if (contextParts.isEmpty() && !databaseIsLocal(liquibase)) {
+            throw new IllegalStateException("Running context-less migration in qa or prod ist verboten - that would cause all contexts to run which is only allowed in local environment.");
+        } else if (contextParts.contains("legacy")) {
+            throw new IllegalStateException("Running 'legacy' context migration ist verboten.\n" +
+                "      These contexts have special meaning and their changesets are never meant to be run in a production environment, nor even locally in isolation.");
+        } else if (contextParts.contains("manual")) {
+            throw new IllegalStateException("Running 'manual' context migration ist verboten.\n" +
+                "      These contexts have special meaning and their changesets are never meant to be run in a production environment, nor even locally in isolation.");
+        } else if (contextParts.contains("blocking")) {
+            throw new IllegalStateException("Running 'blocking' context migration ist verboten.\n" +
+                "      This context has special meaning solely as a marker and its changesets are never meant to be run in isolation.");
+        } else if (contextParts.contains("non-blocking")) {
+            throw new IllegalStateException("Running 'non-blocking' context migration ist verboten.\n" +
+                "      This context has special meaning solely as a marker and its changesets are never meant to be run in isolation.");
+        }
+    }
+
+    private static boolean databaseIsLocal(Liquibase liquibase) {
+        String databaseUrl = liquibase.getDatabase().getConnection().getURL();
+        return databaseUrl.contains("localhost") || databaseUrl.contains("127.0.0.1");
     }
 }
